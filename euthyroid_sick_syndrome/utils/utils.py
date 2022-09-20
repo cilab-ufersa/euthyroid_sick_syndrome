258a03970ff9dd4934af946719af0597f7d87aba
roscibely <rosanacibely@gmail.com>
2022-09-20 20:11:06 -0300

    refactor: refactored prepare_dataset

--- euthyroid_sick_syndrome/utils/utils.py
+++ euthyroid_sick_syndrome/utils/utils.py
@@ -8,6 +8,6 @@ def prepare_dataset(path_origin, columns_labels, path_destiny):
         columns_labels (list): columns label
         path_destiny (string): path destiny
     """
-    df = pd.read_csv(path_origin)
-    df.columns=columns_labels
-    df.to_csv(path_destiny)
+    dataframe = pd.read_csv(path_origin)
+    dataframe.columns=columns_labels
+    dataframe.to_csv(path_destiny, index=False)
