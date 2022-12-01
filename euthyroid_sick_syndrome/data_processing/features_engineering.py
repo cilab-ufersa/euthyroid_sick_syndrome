d5a3f4146325a94f0edcab7ecafc8cc1b2970a6a
roscibely <rosanacibely@gmail.com>
2022-12-01 14:42:26 -0300

    refactor: refactored files

--- euthyroid_sick_syndrome/data_processing/features_engineering.py
+++ euthyroid_sick_syndrome/data_processing/features_engineering.py
@@ -5,26 +5,18 @@ import numpy as np
 from sklearn.feature_selection import RFECV
 
 if __name__ == '__main__':
-    
+
     dataset = pd.read_csv('euthyroid_sick_syndrome\datasets\euthyroid\euthyroid_final_dataset.csv')
-
-    output_label_dataset = dataset['classification']
-    dataset = dataset.drop(['classification'], axis=1)
-
-    # Finding Correlated Features
 
     """
     The effect of collinearity is that your model will overfit
     """
+    # Removing Correlated Features
+    columns = ['TSH_measured', 'T3_measured', 'TT4_measured', 'T4U_measured', 'FTI_measured']
+    dataset.drop(columns, axis=1, inplace=True)
 
     correlations = dataset.corr()
     sns.heatmap(correlations)
-    #plt.show()
+    plt.show()
 
-
-    # Removing Correlated Features
-    columns = ['TSH_measured', 'T3_measured', 'TT4_measured', 'T4U_measured', 'FTI_measured', 'TBG_measured']
-
-    dataset.drop(columns, axis=1, inplace=True)
-
-    dataset.head()
+    dataset.to_csv('euthyroid_sick_syndrome\datasets\euthyroid\euthyroid_final_features.csv', index=False)
