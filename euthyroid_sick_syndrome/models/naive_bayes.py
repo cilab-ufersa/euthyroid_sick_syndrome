d58c5891cb62391042816d9e3b11b2fef5f407f4
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-01-24 10:38:49 -0300

    feat: add metrics

--- euthyroid_sick_syndrome/models/naive_bayes.py
+++ euthyroid_sick_syndrome/models/naive_bayes.py
@@ -1,12 +1,15 @@
+import sys
+sys.path.append('euthyroid_sick_syndrome\euthyroid_sick_syndrome')
 import pandas as pd #Para trabalhar com dataframes               
 import numpy as np #Para trabalhar com arrays
 import matplotlib.pyplot as plt #Para plotar os gráficos
-from sklearn.naive_bayes import GaussianNB #Para criar o modelo de decisão
+from sklearn.naive_bayes import GaussianNB, CategoricalNB #Para criar o modelo de decisão
 from sklearn.model_selection import train_test_split #Para dividir o dataset em treino e teste
 import seaborn as sns 
 from imblearn.over_sampling import SMOTE #Para balancear o dataset
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay #Para plotar a matriz de confusão
 import pickle #para organizar e salvar variaveis em um conjunto
+from utils import * 
 
 if __name__ == "__main__":
     #Carregando o dataset
@@ -30,7 +33,7 @@ if __name__ == "__main__":
         pickle.dump([input_train, output_train], f)
 
     #Criando e implementando o modelo de decisão
-    model = GaussianNB()
+    model = GaussianNB(var_smoothing=1e-5)
     model.fit(input_train, output_train) #Treinamento
 
     #model.classes_ #classes do banco de dados 
@@ -40,11 +43,23 @@ if __name__ == "__main__":
     # Fazer a classificação 
     output_model_decision = model.predict(input_test)
 
+    
+
     #Plotando a matriz de confusão
-    cm = confusion_matrix(output_test, output_model_decision)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
-    disp.plot()
-    disp.ax_.set_title('Matriz de confusão')
-    disp.ax_.set_xlabel('Classificação prevista')
-    disp.ax_.set_ylabel('Classificação real')
-    plt.show()
+    plot_confusion_matrix(output_test, output_model_decision, model)
+
+    accuracy(output_test, output_model_decision) #Pontuação de acurácia
+    
+    precision(output_test, output_model_decision) #Pontuação de precisão
+
+    recall(output_test, output_model_decision) #Pontuação de recall
+
+    f1(output_test, output_model_decision)
+
+    
+    roc(output_test, output_model_decision) #plotando a curva ROC
+
+    #plotando a curva de erro
+    miss_classification(input_train, output_train, input_test, output_test, model)
+
+    learning_curves(input_train, output_train, input_test, output_test, model)
