b18bff9a04ff878cb058a27b4cbce2cc8a4e9680
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-03-11 17:05:33 -0300

    feat: add XGBoost classifier

---
+++ euthyroid_sick_syndrome/models/XGBoost.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append('euthyroid_sick_syndrome')
+
+import pandas as pd #Para trabalhar com dataframes               
+from xgboost import XGBClassifier #Para criar o modelo de árvore de decisão
+
+#Biblioteca para visualização
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+from utils import *
+#from sklearn.model_selection import RandomizedSearchCV
+import joblib
+
+if __name__ == '__main__':
+
+    #Carregando o dataset
+    dataset = pd.read_csv('euthyroid_sick_syndrome/datasets/euthyroid/euthyroid_final_features.csv')  
+    output_label_dataset = dataset['classification']  #1 = sick, 0 = normal
+    dataset = dataset[['age', 'sex', 'sick', 'TSH', 'T3', 'TT4', 'T4U', 'FTI']]
+
+    #Balanceamento dos dados 
+    dataset_res, ouput_label = balance_dataset_smote(dataset, output_label_dataset, random_state=42, k_neighbors=5)
+
+    #Dividindo o dataset em treino e teste
+    #80 % para treino e 20% para teste
+    input_train, input_test, output_train, output_test = slipt_and_standardize_dataset(dataset=dataset_res, output_label=ouput_label)
+
+    model = XGBClassifier(n_estimators = 650, max_depth = 10, learning_rate = 0.01, subsample = 1, random_state=0)
+    model.fit(input_train, output_train) #Treinamento
+
+    #
+    #joblib.dump(model, 'XGBoostClassifier.sav')
+
+    # Fazer a classificação
+    output_model_decision = model.predict(input_test)
+
+    #Plotando
+
+    plot_confusion_matrix(output_test, output_model_decision, model)
+
+    accuracy(output_test, output_model_decision) #Pontuação de acurácia
+    
+    precision(output_test, output_model_decision) #Pontuação de precisão
+
+    recall(output_test, output_model_decision) #Pontuação de recall
+
+    f1(output_test, output_model_decision)
+    
+    roc(output_test, output_model_decision) #plotando a curva ROC
+ 
+    #plotando a curva de erro
+    miss_classification(input_train, output_train, input_test, output_test, model)
+
+    learning_curves(input_train, output_train, input_test, output_test, model)
