3b0ac2d9bdbefc653a96f01d9a95c4dede0ccbde
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-03-18 19:31:46 -0300

    feat: Grid search added in XGBoost model

--- euthyroid_sick_syndrome/models/XGBoost.py
+++ euthyroid_sick_syndrome/models/XGBoost.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from utils import *
-#from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
 import joblib
 
 if __name__ == '__main__':
@@ -26,10 +27,33 @@ if __name__ == '__main__':
     #80 % para treino e 20% para teste
     input_train, input_test, output_train, output_test = slipt_and_standardize_dataset(dataset=dataset_res, output_label=ouput_label)
 
-    model = XGBClassifier(n_estimators = 650, max_depth = 10, learning_rate = 0.01, subsample = 1, random_state=0)
+    # Definindo o espaço de busca 
+    param_grid_xgb = { 
+        # Porcentagem de colunas a serem amostradas aleatoriamente para cada arvore.
+        "colsample_bytree": [ 0.3, 0.5 , 0.8 ],
+        # reg_alpha promove a regularização l1 para o peso, valores mais altos resultam em modos mais conservadores
+        "reg_alpha": [0, 0.5, 1, 5],
+        # reg_lambda promove a regularização l2 para o peso, valores mais altos resultam em modos mais conservadores
+        "reg_lambda": [0, 0.5, 1, 5]
+        }
+
+    # Configuração de pontuação
+    scoring = ['recall']
+
+    # Configuração para validação cruzada
+    kfold = StratifiedKFold(n_splits=3, shuffle=True, random_state=0)
+
+    # estimador - Significa que estamos usando o XGBoost como modelo.
+    # param_grid_xgb - Leva nosso espaço de pesquisa pré-definido para a pesquisa em grade.
+    # scoring - Define a metrica de avaliação de desempenho. Como definimos a pontuação como 'recall', o modelo usará o recall como metrica de avaliação.
+    # refit - Permite reajustar o modelo com os melhores parâmetros em todo o conjunto de dados de treinamento.
+    # n_jobs - Significa que o processamento paralelo esta usando todos os processadores.
+    # cv - Leva o que definimos para StratifiedkFold
+    # verbose - controla o numero de mensagens retornadas pela pesquisa em grade. Quanto maior o número, mais informações são retornadas. Se verbose = 0, isso significa uma saida silenciosa.
+    # Após ajustar GridSearchCV no conjunto de dados de treinamento, teremos 48 combinações de hiperparâmetros. Como a validação cruzada de 3 vezes é usada, existem 144 modelos treinados no total.
+    model = GridSearchCV(estimator=XGBClassifier(), param_grid=param_grid_xgb, scoring=scoring, refit='recall', n_jobs=-1, cv=kfold, verbose=0)
     model.fit(input_train, output_train) #Treinamento
 
-    #
     #joblib.dump(model, 'XGBoostClassifier.sav')
 
     # Fazer a classificação
@@ -37,7 +61,7 @@ if __name__ == '__main__':
 
     #Plotando
 
-    plot_confusion_matrix(output_test, output_model_decision, model)
+    plot_confusion_matrix(output_test, output_model_decision, model, title = 'Matriz Confusão')
 
     accuracy(output_test, output_model_decision) #Pontuação de acurácia
     
