c34bd68c6e7d5b7edb94926392fde763c1b46101
vinicius-a-almeida <vinicius45anacleto@gmail.com>
2023-04-26 21:55:53 -0300

    feat: tratando valores e selecionando features

--- euthyroid_sick_syndrome/data_processing/features_engineering.py
+++ euthyroid_sick_syndrome/data_processing/features_engineering.py
@@ -15,7 +15,17 @@ if __name__ == '__main__':
     columns = ['TSH_measured', 'T3_measured', 'TT4_measured', 'T4U_measured', 'FTI_measured']
     dataset.drop(columns, axis=1, inplace=True)
     dataset['sex']= dataset['sex'].astype("category").cat.codes.values 
-    dataset['sick']= dataset['sick'].astype("category").cat.codes.values 
+    dataset['sick']= dataset['sick'].astype("category").cat.codes.values
+    dataset['on_thyroxine'] = dataset['on_thyroxine'].astype("category").cat.codes.values
+    dataset['query_on_thyroxine'] = dataset['query_on_thyroxine'].astype("category").cat.codes.values
+    dataset['on_antithyroid_medication'] = dataset['on_antithyroid_medication'].astype("category").cat.codes.values
+    dataset['thyroid_surgery']= dataset['thyroid_surgery'].astype("category").cat.codes.values
+    dataset['query_hypothyroid']= dataset['query_hypothyroid'].astype("category").cat.codes.values
+    dataset['query_hyperthyroid']= dataset['query_hyperthyroid'].astype("category").cat.codes.values
+    dataset['pregnant']= dataset['pregnant'].astype("category").cat.codes.values
+    dataset['tumor']= dataset['tumor'].astype("category").cat.codes.values
+    dataset['lithium']= dataset['lithium'].astype("category").cat.codes.values
+    dataset['goitre']= dataset['goitre'].astype("category").cat.codes.values
     
     correlations = dataset.corr()
     sns.heatmap(correlations)
--- euthyroid_sick_syndrome/models/select_features.py
+++ euthyroid_sick_syndrome/models/select_features.py
@@ -6,13 +6,9 @@ from sklearn.feature_selection import RFE
 from utils import *
 
 #Carregando o dataset
-dataset = pd.read_csv('euthyroid_sick_syndrome/datasets/euthyroid/euthyroid_final_features.csv')
+dataset = pd.read_csv('euthyroid_sick_syndrome\datasets\euthyroid\euthyroid_final_features.csv')
 output_label_dataset = dataset['classification']
 dataset = dataset.drop(['classification'], axis=1)
-#output_label_dataset = dataset['classification']  #1 = sick, 0 = normal
-#dataset = dataset[['age', 'sex', 'sick', 'TSH', 'T3', 'TT4', 'T4U', 'FTI']]
-#dataset = dataset[['classification', 'age', 'sex','on_thyroxine','query_on_thyroxine','on_antithyroid_medication','thyroid_surgery','query_hypothyroid','query_hyperthyroid','pregnant', 'sick', 'tumor','lithium', 'goitre', 'TSH', 'T3', 'TT4', 'T4U', 'FTI']]
-
 
 #Balanceamento dos dados 
 dataset_res, ouput_label = balance_dataset_smote(dataset, output_label_dataset, random_state=42, k_neighbors=5)
@@ -22,15 +18,11 @@ dataset_res, ouput_label = balance_dataset_smote(dataset, output_label_dataset, 
 input_train, input_test, output_train, output_test = slipt_and_standardize_dataset(dataset=dataset_res, output_label=ouput_label)
                                                        
 
-
-
-
 model = RandomForestClassifier(class_weight = 'balanced_subsample', criterion = 'log_loss',
 max_depth = 10, min_samples_split = 2, n_estimators = 10, random_state = 10,
 max_features ='sqrt', min_samples_leaf=5)
 model.fit(input_train, output_train)
-print("importância das features: ", model.feature_importances_)
-
+print("importância das features: \n", model.feature_importances_)
 
 
 # Definir o RFE com 3 recursos a serem selecionados, ou seja ele está selecionando as 3 melhores features
@@ -39,19 +31,18 @@ rfe = RFE(estimator=model, n_features_to_select=3)
 # Executar o RFE no conjunto de treinamento
 rfe.fit(input_train, output_train)
 
-# Verificar os recursos selecionados
-print("Verificar os recursos selecionados")
-print(rfe.support_)
-print(rfe.ranking_)
+# Verificando os recursos selecionados
+for i in range(input_train.shape[1]):
+ print('Coluna: %d, Selecionado %s, Rank: %.3f' % (i, rfe.support_[i], rfe.ranking_[i]))
+
 # Selecionar os recursos do conjunto de treinamento e teste
 input_train_rfe = rfe.transform(input_train)
 X_test_rfe = rfe.transform(input_test)
 
-# Criar um novo modelo com os recursos selecionados
+# Criando um novo modelo com os recursos selecionados
 model.fit(input_train_rfe, output_train)
 
-# Avaliar a precisão do modelo no conjunto de teste
-
+# Avaliando a acuracia do modelo no conjunto de teste
 
 y_pred = model.predict(X_test_rfe)
 accuracy(output_test, y_pred)
