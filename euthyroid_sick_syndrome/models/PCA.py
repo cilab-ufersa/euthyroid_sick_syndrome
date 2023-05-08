8957f1df800b5206eb4143e4d759f6cad173ce6f
vinicius Almeida <vinicius45anacleto@gmail.com>
2023-05-08 09:12:03 -0300

    feat: pca

--- euthyroid_sick_syndrome/models/PCA.py
+++ euthyroid_sick_syndrome/models/PCA.py
@@ -1,10 +1,6 @@
 import sys
 sys.path.append('euthyroid_sick_syndrome')
-from sklearn.cluster import KMeans
-from sklearn.feature_selection import SelectFromModel
-from sklearn.linear_model import LassoCV
-import numpy as np
-import pandas as pd
+from sklearn.decomposition import PCA
 from utils import *
 
 # carregando o dataset
@@ -19,16 +15,20 @@ dataset_res, ouput_label = balance_dataset_smote(dataset, output_label_dataset, 
 input_train, input_test, output_train, output_test = slipt_and_standardize_dataset(dataset=dataset_res, output_label=ouput_label)
 
 
-# Criando clusters usando KMeans
-kmeans = KMeans(n_clusters = 5, random_state = 42)
-kmeans.fit(input_train)
+# Criação do objeto PCA e especificando o número de componentes
+pca = PCA(n_components = 2)
 
-# Selecionando recursos importantes usando LassoCV
-# Selecionando 8 features com o parametro max_features=8
-lasso = LassoCV(cv=5)
-selection = SelectFromModel(estimator=lasso, threshold=-np.inf, max_features = 8)
-selection.fit(input_train, kmeans.labels_)
-selected_features = selection.get_support(indices=True)
+# Aplicação da PCA aos dados
+pca.fit(input_train)
 
-# Imprimindo os recursos selecionados
-print("Recursos selecionados: ", dataset.columns[selected_features])
+# Transformando os dados para as novas dimensões
+X_transformed = pca.transform(input_train)
+
+# Imprimindo os dados transformados
+print(X_transformed)
+
+import matplotlib.pyplot as plt
+
+# Plotando os dados transformados
+plt.scatter(X_transformed[:,0], X_transformed[:,1])
+plt.show()
